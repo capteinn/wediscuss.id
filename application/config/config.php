f694dadfd976b5944c6167244ccf5d0ffc9daf7e
Abdurrahman <capteinn@gmail.com>
2018-10-06 06:20:32 +0700

    Add dinamic root

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,9 @@ date_default_timezone_set("Asia/Bangkok");
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$_root  = "http://".$_SERVER['HTTP_HOST'];
+$_root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+$config['base_url']	= $_root;
 
 /*
 |--------------------------------------------------------------------------
