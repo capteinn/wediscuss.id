6b7a14a6f81cf56431593e4c900c97f2234e4950
Abdurrahman <capteinn@gmail.com>
2018-10-06 07:29:30 +0700

    direct to landing page

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,8 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 	public function index()
 	{
-		$this->load->view('layouts.header');
+		$this->load->view('layouts/header');
 		$this->load->view('home');
-		$this->load->view('layouts.footer');
+		$this->load->view('layouts/footer');
 	}
 }
