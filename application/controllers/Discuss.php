d699579297765133a11b68a8f00d77359c854bf7
Abdurrahman <capteinn@gmail.com>
2018-10-06 09:53:07 +0700

    add discuss controller

---
+++ application/controllers/Discuss.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Discuss extends CI_Controller {
+	/**
+     * This is default constructor of the class
+     */
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('thread_model');
+	}
+
+	public function index()
+	{
+		$data['threads'] = $this->thread_model->all();
+
+		$this->load->view('layouts/header_user');
+		$this->load->view('user/discuss', $data);
+		$this->load->view('layouts/footer_user');
+	}
+}
