71caba3a1e3fa620e3bd61f35c3aa5e4ae9455ba
Abdurrahman <capteinn@gmail.com>
2018-10-06 07:48:37 +0700

    make controller dashboard

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+	public function index()
+	{
+		$this->load->view('layouts/header_admin');
+		$this->load->view('user/dashboard');
+		$this->load->view('layouts/footer_admin');
+	}
+}
