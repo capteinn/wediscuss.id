742917fcc28fb99475ce7c7ef8bf635ee8645f60
Abdurrahman <capteinn@gmail.com>
2018-10-07 18:56:43 +0700

    slicing page title and breadcrump

--- application/views/layouts/header_user.php
+++ application/views/layouts/header_user.php
@@ -73,3 +73,20 @@
 <?php $this->load->view('layouts/menu_user') ?>
 <!-- partial -->
 <div class="main-panel">
+  <div class="content-wrapper">
+    <div class="page-header">
+      <h3 class="page-title">
+        <span class="page-title-icon bg-gradient-primary text-white mr-2">
+          <i class="mdi mdi-home"></i>                 
+        </span>
+        <?= !empty($title) ? $title : 'Undefined Title'; ?>
+      </h3>
+      <nav aria-label="breadcrumb">
+        <ul class="breadcrumb">
+          <li class="breadcrumb-item active" aria-current="page">
+            <span></span>Overview
+            <i class="mdi mdi-alert-circle-outline icon-sm text-primary align-middle"></i>
+          </li>
+        </ul>
+      </nav>
+    </div>
