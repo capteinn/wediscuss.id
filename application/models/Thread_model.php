134993c1bfd7092f9ac7999df4d83d534fb572ce
Abdurrahman <capteinn@gmail.com>
2018-10-08 02:44:18 +0700

    add function sote

--- application/models/Thread_model.php
+++ application/models/Thread_model.php
@@ -5,4 +5,8 @@ class Thread_model extends CI_Model
 	function all() {
 		return $this->db->join('users', 'users.id = threads.user_id')->get('threads')->result();
 	}
+
+	function store($data) {
+		return $this->db->insert('threads', $data);
+	}
 }
