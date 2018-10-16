9ab23d49dbd6137becb9552308882a155a7aab86
Abdurrahman <capteinn@gmail.com>
2018-10-16 17:10:17 +0700

    add detail function model

--- application/models/Thread_model.php
+++ application/models/Thread_model.php
@@ -6,6 +6,10 @@ class Thread_model extends CI_Model
 		return $this->db->select('users.username, users.photo, threads.created_at date_realease, threads.*')->join('users', 'users.id = threads.user_id')->get('threads')->result();
 	}
 
+	function detail($id) {
+		return $this->db->select('users.username, users.photo, threads.created_at date_realease, threads.*')->join('users', 'users.id = threads.user_id')->where('threads.id', $id)->get('threads')->row();
+	}
+
 	function store($data) {
 		return $this->db->insert('threads', $data);
 	}
