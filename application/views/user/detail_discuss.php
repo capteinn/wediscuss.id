c6e6ead6969381ec90bd8e32bf8f59b6024a0fc5
Abdurrahman <capteinn@gmail.com>
2018-10-16 17:09:56 +0700

    add page discuss detail

---
+++ application/views/user/detail_discuss.php
@@ -0,0 +1,40 @@
+<div class="row">
+  <div class="col-12 grid-margin stretch-card">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Highlighted Text</h4>
+        <p class="card-description">
+          21 Agustus 20018
+        </p>
+        <p>
+          It is a long fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution
+        </p>
+        <p>
+          <i class="mdi mdi-compass icon-sm text-danger"></i>
+          Categories
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-12 grid-margin stretch-card">
+    <div class="card">
+      <div class="card-body">
+        <p class="text-muted float-right"><i class="mdi mdi-wechat icon-sm text-info"></i> 20 Comments</p>
+        <h4 class="card-title">Reply</h4>
+        <hr>
+        <div class="media">
+            <img style="width: 50px; height: 50px" src="<?= base_url() ?>public/images/faces/face4.jpg" alt="image" class="icon-md text-info d-flex align-self-center mr-3 circle-rounded">
+            <div class="media-body">              
+             <p class="card-text">ametamet nibh libero, in gravida nulla. Nulla vel metus scelerisque.</p>
+             <p class="card-text">ametamet nibh libero, in gravida nulla. Nulla vel metus scelerisque.</p>
+             <p class="card-text">ametamet nibh libero, in gravida nulla. Nulla vel metus scelerisque.</p>
+            </div>
+            
+        </div>
+        <hr>
+      </div>
+    </div>
+  </div>
+</div>
