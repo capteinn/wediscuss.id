f1a81209602d0f961f2deff41fb1a3fa3402d37a
Abdurrahman <capteinn@gmail.com>
2018-10-24 13:23:13 +0700

    edit thread model

--- application/models/Thread_model.php
+++ application/models/Thread_model.php
@@ -7,7 +7,7 @@ class Thread_model extends CI_Model
 	}
 
 	function detail($id) {
-		return $this->db->select('users.username, users.photo, threads.created_at date_realease, threads.*')->join('users', 'users.id = threads.user_id')->where('threads.id', $id)->get('threads')->row();
+		return $this->db->select('users.username, users.photo, threads.created_at date_realease, threads.*, categories.name category_name, threads.id thread_id')->join('users', 'users.id = threads.user_id')->join('categories', 'categories.id = threads.category_id')->where('threads.id', $id)->get('threads')->row();
 	}
 
 	function store($data) {
