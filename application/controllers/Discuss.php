400817d958e7129468e0ed185885da8649dbee4c
Abdurrahman <capteinn@gmail.com>
2018-10-24 13:23:31 +0700

    add comment model

--- application/controllers/Discuss.php
+++ application/controllers/Discuss.php
@@ -10,6 +10,7 @@ class Discuss extends CI_Controller {
 		parent::__construct();
 		$this->load->model('thread_model');
 		$this->load->model('categories_model');
+		$this->load->model('comment_model');
 	}
 
 	public function index()
