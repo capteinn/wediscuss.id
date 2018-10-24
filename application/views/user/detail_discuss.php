ba00a2e6b9492ebf98fa100768765f578d0f0c88
Abdurrahman <capteinn@gmail.com>
2018-10-24 13:22:57 +0700

    edit detail discuss page

--- application/views/user/detail_discuss.php
+++ application/views/user/detail_discuss.php
@@ -2,16 +2,25 @@
   <div class="col-12 grid-margin stretch-card">
     <div class="card">
       <div class="card-body">
-        <h4 class="card-title">Highlighted Text</h4>
+        <h4 class="card-title"><?php echo $details->title ?></h4>
+        <p class="float-right">
+          <i class="mdi mdi-thumb-up text-danger"></i> <?php echo $details->like ?>
+        </p>
         <p class="card-description">
-          21 Agustus 20018
+          <?php echo date('d F Y H:i', strtotime($details->date_realease)) ?>
         </p>
         <p>
-          It is a long fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution
+          <?php echo $details->description ?>
         </p>
         <p>
           <i class="mdi mdi-compass icon-sm text-danger"></i>
-          Categories
+          <?php echo $details->category_name ?>
+          <button type="button" title="Unlike" class="mx-2 btn btn-outline-secondary btn-rounded btn-icon float-right">
+            <i class="mdi mdi-thumb-down text-dark"></i>
+          </button>
+          <button type="button" title="Like" class="mx-2 btn btn-outline-secondary btn-rounded btn-icon float-right">
+            <i class="mdi mdi-thumb-up text-danger"></i>
+          </button>
         </p>
       </div>
     </div>
@@ -24,15 +33,53 @@
         <p class="text-muted float-right"><i class="mdi mdi-wechat icon-sm text-info"></i> 20 Comments</p>
         <h4 class="card-title">Reply</h4>
         <hr>
-        <div class="media">
-            <img style="width: 50px; height: 50px" src="<?= base_url() ?>public/images/faces/face4.jpg" alt="image" class="icon-md text-info d-flex align-self-center mr-3 circle-rounded">
-            <div class="media-body">              
-             <p class="card-text">ametamet nibh libero, in gravida nulla. Nulla vel metus scelerisque.</p>
-             <p class="card-text">ametamet nibh libero, in gravida nulla. Nulla vel metus scelerisque.</p>
-             <p class="card-text">ametamet nibh libero, in gravida nulla. Nulla vel metus scelerisque.</p>
+        <div class="media d-block">
+          <form action="" method="POST" role="form">
+            <div class="row">
+              <div class="col-11 mr-0">
+                <div class="form-group">
+                  <textarea class="form-control" name="description" id="description" required="" placeholder="Isikan komentar Anda"></textarea>
+                </div>
+              </div>
+              <div class="form-group mt-2">
+                <button class="btn btn-primary btn-sm float-right">Reply</button>
+              </div>
             </div>
-            
+          </form>
         </div>
+        <?php
+
+          $comments = $this->comment_model->detail($details->thread_id);
+          
+          if (!empty($comments)) {
+
+          foreach ($comments as $comment) {
+
+        ?>
+        <div class="media">
+            <img style="width: 50px; height: 50px" src="<?= base_url() ?>public/images/faces/face4.jpg" alt="image" class="icon-md text-info d-flex align-self-center mx-3 rounded">
+            <div class="media-body">
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+             cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+             proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </div>
+        <hr>
+        <?php 
+            }
+          } else {
+        ?>
+          <div class="media">
+              <div class="media-body">
+                <p class="card-text text-center text-muted">There is no comment in this thread.</p>
+              </div>
+          </div>        
+        <?php   
+          }
+        ?>
         <hr>
       </div>
     </div>
