1a25d27b99fd60aadf9af31d2a8eb89280d67f18
Abdurrahman <capteinn@gmail.com>
2018-12-26 04:49:59 +0700

    add login model

---
+++ application/models/Login_model.php
@@ -0,0 +1,24 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Login_model extends CI_Model
+{
+  function login($username, $password)
+  {
+      $user = $this->db->select('u.id, u.username, u.password, u.role_id, r.name')
+      ->from('users as u')
+      ->join('roles as r','r.id=u.role_id')
+      ->where('u.username', $username)
+      ->where('u.role_id', 1)
+      ->get()->row();
+      
+      if(!empty($user)){
+          if(password_verify($password, $user->password)) {
+              return $user;
+          } else {
+              return array();
+          }
+      } else {
+          return array();
+      }
+  }
+}
