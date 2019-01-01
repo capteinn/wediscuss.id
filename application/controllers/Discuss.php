9723b4e6fa1777c8953ae94c1e4f81e3212a6911
Abdurrahman <capteinn@gmail.com>
2019-01-02 05:06:30 +0700

    add function comment

--- application/controllers/Discuss.php
+++ application/controllers/Discuss.php
@@ -31,6 +31,7 @@ class Discuss extends BaseController {
 	public function detail($id)
 	{
 		$data['details'] = $this->thread_model->detail($id);
+		$data['getLike'] = $this->thread_model->getLike($id, $this->user_id);
 		$data['title'] = 'Detail Discussion';
 		
 		$this->load->view('layouts/header_user', $data);
@@ -79,5 +80,41 @@ class Discuss extends BaseController {
 				$this->add();
 			}
     }
+	}
+
+	public function like($id) {
+		$data = array(
+			'thread_id' => $id,
+			'user_id' => $this->user_id,
+			'like' => 1
+		);
+		$this->thread_model->like($data);
+		$this->thread_model->pushLike($id);
+
+		redirect('discuss/detail/'.$id);
+	}
+
+	public function dislike($id) {
+		$data = array(
+			'thread_id' => $id,
+			'user_id' => $this->user_id,
+		);
+		$this->thread_model->dislike($data);
+		$this->thread_model->pushDislike($id);
+
+		redirect('discuss/detail/'.$id);
+	}
+
+	public function comment() {
+		$id = $this->input->post('id');
+
+		$data = array(
+			'thread_id' => $id,
+			'description' => $this->input->post('description'),
+			'user_id' => $this->user_id
+		);
+		$this->comment_model->store($data);
+
+		redirect('discuss/detail/'.$id);
 	}
 }
